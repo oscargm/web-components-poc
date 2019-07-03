f5e78bda79836da7bfa16a461e172fb8f9d0a827
Oscar Garcia <oscar.garcia.og3@roche.com>
2019-07-03 18:55:30 +0200

    changed lit-element import

--- react/src/app.jsx
+++ react/src/app.jsx
@@ -1,2 +1,13 @@
-import * as React from 'react';
-export const App = () => <h1>Hello</h1>;
+import * as React from "react";
+import { TodoApp } from "./web-component/to-do-app";
+
+customElements.define("to-do-app", TodoApp);
+
+const todos = [
+  { text: "Native WC", checked: true },
+  { text: "React WC", checked: false },
+  { text: "VueJS WC", checked: false },
+  { text: "Svelte WC", checked: false },
+  { text: "Angular WC", checked: false }
+];
+export const App = () => <to-do-app todos={todos} />;
