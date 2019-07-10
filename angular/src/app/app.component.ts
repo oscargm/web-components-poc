c617db7c1e60a623101877fa1b5eaf9ee1d9ea39
Oscar Garcia <oscar.garcia.og3@roche.com>
2019-07-10 10:46:37 +0200

    angular working

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import "../components/to-do-app";
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "angular";
+}
---
+++ svelte-component/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import TodoApp from './component/to-do-app.js';
+import { register } from 'svelte-custom-elements';
+
+const todos = [
+        { text: "Native WC", checked: true },
+        { text: "React WC", checked: false },
+        { text: "VueJS WC", checked: false },
+        { text: "Svelte WC", checked: false },
+        { text: "Angular WC", checked: false }
+      ];
+register( 'to-do-app', TodoApp, [todos] );
+
+//document.body.innerHTML = '<my-component></my-component>';
+</script>
+<to-do-app todos></to-do-app>
